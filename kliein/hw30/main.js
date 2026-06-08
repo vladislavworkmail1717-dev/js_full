30b7914a1ef94ffbc082c3fa0702287b3603174e
Ihor <klejnigor5@gmail.com>
2026-06-09 00:35:59 +0300

    addhw30

---
+++ kliein/hw30/main.js
@@ -0,0 +1,43 @@
+console.log('#11. JavaScript homework example file')
+
+/*
+ * #1
+ *
+ * Написати функцію, яка приймає рядок як вхідний параметр і перевіряє, чи є цей рядок валідною електронною адресою за допомогою регулярного виразу.
+ * Функція повертає true, якщо електронна адреса валідна, і false в іншому випадку.
+ *
+ */
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+
+}
+console.log(isValidEmail('example@example.com')); // Повинно вивести: true
+console.log(isValidEmail('invalid-email'));       // Повинно вивести: false
+
+/*
+ * #2
+ *
+ * Написати функцію, яка приймає рядок як вхідний параметр і перевіряє, чи є цей рядок валідним URL веб-сайту за допомогою регулярного виразу.
+ * Функція повертає true, якщо URL валідний, і false в іншому випадку.
+ *
+ */
+
+/*
+
+*/
+
+function isValidUrl(url) {
+  const urlRegex = /^(https?:\/\/)?([a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,})(\/[^\s]*)?$/;
+  return urlRegex.test(url);
+
+}
+
+console.log(isValidUrl('https://www.example.com')); // Повинно вивести: true
+console.log(isValidUrl('invalid-url'));             // Повинно вивести: false
+
+// Експорт функції для використання та тестування
+export { isValidEmail, isValidUrl }
+
+
